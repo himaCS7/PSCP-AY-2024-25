5051f124f8002cdc62645549b0e2cdfc16c70a5f
himaCS7 <himabinduk@nitandhra.ac.in>
2024-10-29 18:36:50 +0530

    halfReverse updated for handling odd sized lists

--- mid/halfReverse.c
+++ mid/halfReverse.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 void swap(int *x, int*y)
 {
 	int tmp = *x;
@@ -10,12 +9,19 @@ void swap(int *x, int*y)
 void halfReverse(int a[ ], int n)
 {
 	int i,j,mid=n/2;
-	for(i=0, j=mid; i<mid; i++, j++)
-		swap(&a[i], &a[j]);
+	printf("mid = %d\n",mid);
+	if(n%2 == 0) //even number of elements
+		for(i=0, j=mid; i<mid; i++, j++)
+			swap(&a[i], &a[j]);
+	else  // odd number of elements
+		for(i=0, j=mid+1; i<mid; i++, j++)
+			swap(&a[i], &a[j]);
+	
 }
 
 int main() {
-    int a[]={1, 2, 3, 4, 5, 6, 7, 8}, n = 8;  // assume n is even
+    int a[]={1, 2, 3, 4, 5, 6, 7, 8}, n = 8;  
+    //int a[]={1, 2, 3, 4, 5, 6, 7, 8, 9}, n = 9;  
 	int i;
 	printf("\narray initially: ");
 	for(i=0; i<n; i++)
