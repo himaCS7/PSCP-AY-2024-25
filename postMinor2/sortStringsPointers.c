5bcdd45d9d61596c174284117faadb32e4ecf626
himaCS7 <himabinduk@nitandhra.ac.in>
2024-11-19 10:39:59 +0530

    strings sorting

--- postMinor2/sortStringsPointers.c
+++ postMinor2/sortStringsPointers.c
@@ -1,27 +1,28 @@
-// Sort strings (lexicographic order = dictionary order) using array of strings, without pointers
+// Sort strings using array of pointers
 #include <stdio.h>
 #include <string.h>
 
 // shall we sort in ascending order
-void insertionSort(char s[][20], int n)
+void insertionSort(char *s[], int n)
 {
 	int i, j;
-	char p[20];
+	char *p;
 	for (i = 1; i < n; i++)
 	{
-		strcpy(p, s[i]); // insert string p in the sorted list s[0..i-1]
+		p = s[i];
+		// insert element p in the sorted list s[0..i-1]
 		for (j = i - 1; j >= 0 && strcmp(s[j], p) > 0; j--)
 		{
-			strcpy(s[j + 1], s[j]); // move to higher index as it is larger in lexicographic order
+			s[j + 1] = s[j];
 		}
 		// place p at (j+1)th position
-		strcpy(s[j + 1], p);
+		s[j + 1] = p;
 	}
 }
 
 int main()
 {
-	char s[10][20] = {"January", "February", "March", "April", "May", "June"};
+	char *s[] = {"January", "February", "March", "April", "May", "June"};
 	int n = 6;
 	printf("The array of strings initially: ");
 	for (int i = 0; i < n; i++)
